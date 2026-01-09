9b6ec6e962da9088d49bffdc5238bb42fe6031de
Michaela Cabrera Kalman <84144152+MikaCK@users.noreply.github.com>
2026-01-09 14:26:26 +0000

    new changes

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,67 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from djongo import models
+
+from django.conf import settings
+from pymongo import MongoClient
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **options):
+        # Connect to MongoDB
+        client = MongoClient('localhost', 27017)
+        db = client['octofit_db']
+
+        # Drop collections if they exist
+        db.users.drop()
+        db.teams.drop()
+        db.activities.drop()
+        db.leaderboard.drop()
+        db.workouts.drop()
+
+        # Users (superheroes)
+        users = [
+            {"name": "Superman", "email": "superman@dc.com", "team": "DC"},
+            {"name": "Batman", "email": "batman@dc.com", "team": "DC"},
+            {"name": "Wonder Woman", "email": "wonderwoman@dc.com", "team": "DC"},
+            {"name": "Iron Man", "email": "ironman@marvel.com", "team": "Marvel"},
+            {"name": "Captain America", "email": "cap@marvel.com", "team": "Marvel"},
+            {"name": "Black Widow", "email": "widow@marvel.com", "team": "Marvel"},
+        ]
+        db.users.insert_many(users)
+        db.users.create_index([("email", 1)], unique=True)
+
+        # Teams
+        teams = [
+            {"name": "Marvel", "members": ["Iron Man", "Captain America", "Black Widow"]},
+            {"name": "DC", "members": ["Superman", "Batman", "Wonder Woman"]},
+        ]
+        db.teams.insert_many(teams)
+
+        # Activities
+        activities = [
+            {"user": "Superman", "activity": "Flying", "duration": 120},
+            {"user": "Batman", "activity": "Martial Arts", "duration": 90},
+            {"user": "Iron Man", "activity": "Suit Training", "duration": 60},
+            {"user": "Captain America", "activity": "Shield Practice", "duration": 45},
+        ]
+        db.activities.insert_many(activities)
+
+        # Leaderboard
+        leaderboard = [
+            {"user": "Superman", "points": 1000},
+            {"user": "Iron Man", "points": 950},
+            {"user": "Batman", "points": 900},
+        ]
+        db.leaderboard.insert_many(leaderboard)
+
+        # Workouts
+        workouts = [
+            {"user": "Superman", "workout": "Strength", "reps": 100},
+            {"user": "Wonder Woman", "workout": "Agility", "reps": 80},
+            {"user": "Black Widow", "workout": "Stealth", "reps": 70},
+        ]
+        db.workouts.insert_many(workouts)
+
+        self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data.'))
